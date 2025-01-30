548457374e3aac5b9a0a2ad0c3af88ced3e0bcbb
ikumar55 <147382138+ikumar55@users.noreply.github.com>
2025-01-29 18:46:02 -0800

    changes

--- global.js
+++ global.js
@@ -4,5 +4,11 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+
+
+
 const navLinks = $$("nav a");
-console.log(navLinks); // Debugging: Check if we successfully got all the links
+// Find the link to the current page
+const currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
