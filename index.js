a6f4e48d09f3540cbc0decfd08996fd1df22abdb
ikumar55 <147382138+ikumar55@users.noreply.github.com>
2025-03-16 13:29:50 -0700

    Better intro, fixed CycleScope image

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         // Fetch GitHub data for your username
-        const githubData = await fetchGitHubData('idhant-kumar'); // Replace with your GitHub username
+        const githubData = await fetchGitHubData('ikumar55'); // Replace with your GitHub username
         console.log('GitHub Data:', githubData);
 
         // Select the container for profile stats
